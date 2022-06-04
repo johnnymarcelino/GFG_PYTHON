6dbaac146c1609b5d969bbe25ebb1278a4b5b6d1
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-06-04 14:34:17 -0300

    Learning Python - Functions

--- Functions.py
+++ Functions.py
@@ -58,7 +58,6 @@ def myFun(*argv):
 
 myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')'''
 
-
 # Variable length keyword arguments
 
 # Python program to illustrate
@@ -79,7 +78,6 @@ myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')'''
 myFun(first='Geeks', mid='for', last='Geeks')
 print(myFun.__doc__)"""
 
-
 # Instrução de retorno de função Python
 
 '''def square_value(num):
@@ -90,7 +88,6 @@ print(myFun.__doc__)"""
 
 print(square_value(5))
 print(square_value(-9))'''
-
 
 '''def swap(x, y):
 	temp = x
@@ -133,11 +130,10 @@ myNewFun(*n)
 varioN = {"first_name": "johnny", "Last_Name": "Marcelino", "Age": 26, "Birthday": 1995}
 myNewFun(*n, **varioN)'''
 
-
 # Global Keyword / Palavra-chave global
 
 # This function modifies the global variable
-def f():
+'''def f():
     global s
     s += " All Languages is greater!"
     print("Inside of function", s)
@@ -147,4 +143,128 @@ def f():
 
 s = "Python is greater!"
 f()
-print("Outside of function", s)
+print("Outside of function", s)'''
+
+'''z = 1
+# Use global because there is no local 's'
+def a():
+    print("Inside a()", z)'''
+
+# Variable "a" is redefined as a local
+'''def b():
+    z = 2
+    print("Inside b()", z)
+
+
+def c():
+    global z
+    z = 3
+    print("inside c()", z)'''
+
+# Global scope
+'''print("global", z)
+a()
+print("Global", z)
+b()
+print("Global", z)
+c()
+print("global", z)
+global z
+print(z)'''
+
+# Python program to modify a global
+# value inside a function
+
+'''x = 15
+
+
+def change():
+    # using a global keyword
+    # global x
+
+    # increment value of a by 5
+    # global a
+    a = x + 5
+    print("Value of x inside a function :", a)
+
+
+change()
+print("Value of x outside a function :", x)
+# print("Value of x outside a function :", x+a)'''
+
+# Global in Nested functions
+
+# Python program showing a use of
+# global in nested function
+
+'''def add():
+    global x
+    x = 15
+
+    def change():
+        global x
+        x = x + x
+
+    print("Before making changing: ", x)
+    print("Making change")
+    change()
+    print("After making change: ", x)
+
+
+# print(f"Value of 'x' before {x}")
+add()
+print("value of x", x)'''
+
+# Funções de primeira classe em Python
+
+# Python program to illustrate functions
+# can be treated as objects
+'''def shout(text):
+    return text.upper()
+
+print(shout('Hello'))
+
+yell = shout
+
+print(yell("4"))'''
+
+# Funções podem ser passadas como argumentos para outras funções:
+
+# Python program to illustrate functions
+# can be passed as arguments to other functions
+'''def shout(text):
+    return text.upper()
+
+
+def whisper(text):
+    return text.lower()
+
+
+def greet(func):
+    # storing the function in a variable
+    greatint = func("""Hi, I am created by a function
+        passed as an argument.""")
+    print(greatint)
+
+
+greet(shout)    # Função de ordem superior
+greet(whisper)  # Função de ordem superior'''
+
+# Funções podem retornar outra função:
+
+# Python program to illustrate functions
+# Functions can return another function
+
+
+'''def create_adder():
+    def adder():
+        return "hello"
+
+    # return adder
+    return adder
+
+
+add_15 = create_adder()
+
+print(add_15())'''
+
